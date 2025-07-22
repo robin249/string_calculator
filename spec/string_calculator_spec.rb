799aa6c0bf3bccd65b71889f6e0e895ae34b8bc6
Robin Garg <er.robin249@gmail.com>
2025-07-22 22:53:21 +0530

    test: return 0 for empty string input

---
+++ spec/string_calculator_spec.rb
@@ -0,0 +1,9 @@
+require 'string_calculator'
+
+RSpec.describe StringCalculator do
+  describe ".add" do
+    it "returns 0 for empty string" do
+      expect(described_class.add("")).to eq(0)
+    end
+  end
+end
