9181f0682fdb612b9e34452a7107c94236157fc5
Robin Garg <er.robin249@gmail.com>
2025-07-22 22:54:57 +0530

    test: return number for single input

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -5,5 +5,9 @@ RSpec.describe StringCalculator do
     it "returns 0 for empty string" do
       expect(described_class.add("")).to eq(0)
     end
+
+    it "returns the number itself for single input" do
+      expect(described_class.add("5")).to eq(5)
+    end
   end
 end
