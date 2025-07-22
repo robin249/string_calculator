98b1d96a4e138fbb8ef796db330b0ded8679e7b6
Robin Garg <er.robin249@gmail.com>
2025-07-22 23:07:42 +0530

    test: support custom delimiter defined in input

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -17,5 +17,9 @@ RSpec.describe StringCalculator do
     it "handles newlines as well as commas" do
       expect(described_class.add("1\n2,3")).to eq(6)
     end
+
+    it "supports custom delimiter in //;\n1;2 format" do
+      expect(described_class.add("//;\n1;2")).to eq(3)
+    end
   end
 end
